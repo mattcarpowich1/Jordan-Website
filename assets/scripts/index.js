db8dd8f8bbaec101ce7e7b3ca3db08831980b227
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-06-11 19:28:31 -0700

    dialing in that data speed

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
   var missionStatement = document.querySelector('.mission-statement');
 
   // initally set position of background (not sure if this is ideal)
-  var Pos = -(window.scrollY / main.dataset.speed); 
+  var Pos = (window.scrollY / main.dataset.speed) * -1; 
   var coords = '50% '+ Pos + 'px';
   main.setAttribute('style', 'background-position: ' + coords);
 
