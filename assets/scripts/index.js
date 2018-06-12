794fc998103554817a05bdfbdf2c4e8c02356edf
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-06-11 19:23:29 -0700

    partial unscratch

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function(event) {
   var main = document.querySelector('main');
   var missionStatement = document.querySelector('.mission-statement');
 
-  // // initally set position of background (not sure if this is ideal)
-  // var Pos = -(window.scrollY / main.dataset.speed); 
-  // var coords = '50% '+ Pos + 'px';
-  // main.setAttribute('style', 'background-position: ' + coords);
+  // initally set position of background (not sure if this is ideal)
+  var Pos = -(window.scrollY / main.dataset.speed); 
+  var coords = '50% '+ Pos + 'px';
+  main.setAttribute('style', 'background-position: ' + coords);
 
   // if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   //   var h = window.innerHeight;
