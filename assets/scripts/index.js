2000f7ccdc0fc035c55a93f25a5ec527a692cee4
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-06-11 20:42:54 -0700

    testimonials. not optimized for mobile, animations are clunky

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,24 +2,18 @@ document.addEventListener('DOMContentLoaded', function(event) {
   var veil = document.querySelector('.veil');
   var main = document.querySelector('main');
   var missionStatement = document.querySelector('.mission-statement');
+  var t1 = document.querySelector('.testimonial-1');
+  var t2 = document.querySelector('.testimonial-2');
+  var t3 = document.querySelector('.testimonial-3');
+
+  var once = false;
+  var once2 = false;
+  var once3 = false;
 
   // initally set position of background (not sure if this is ideal)
   var Pos = (window.scrollY / main.dataset.speed) * -1; 
   var coords = '50% '+ Pos + 'px';
   main.setAttribute('style', 'background-position: ' + coords);
-
-  // if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  //   var h = window.innerHeight;
-  //   main.style.height = h + 'px';
-  //   window.addEventListener('resize', () => {
-  //     var hh = window.innerHeight;
-  //     main.style.height = hh + 'px';
-  //   })
-  //   window.addEventListener('orientationchange', () => {
-  //     var hh = window.innerHeight;
-  //     main.style.height = hh + 'px'
-  //   })
-  // }
   
   window.addEventListener('scroll', function(event) {
     // make main section fade to black
@@ -30,6 +24,21 @@ document.addEventListener('DOMContentLoaded', function(event) {
     var yPos = -(window.scrollY / main.dataset.speed); 
     var coords = '50% '+ yPos + 'px';
     main.setAttribute('style', 'background-position: ' + coords);
+
+    if (t1.getBoundingClientRect().bottom < 880 && !once) {
+      once = true;
+      t1.classList.add('fade-left');
+    } 
+
+    if (t2.getBoundingClientRect().bottom < 880 && !once2) {
+      once2 = true;
+      t2.classList.add('fade-right');
+    } 
+
+    if (t3.getBoundingClientRect().bottom < 880 && !once3) {
+      once3 = true;
+      t3.classList.add('fade-left');
+    } 
   });
 
 });
