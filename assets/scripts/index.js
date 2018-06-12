a5787353fada51c4f0aa435ac5213b8b0462ca2d
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-06-11 18:34:24 -0700

    parallax on main section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(event) { 
   var veil = document.querySelector('.veil');
+  var main = document.querySelector('main');
 
   window.addEventListener('scroll', function(event) {
+    // make main section fade to black
     var blackValue = (Math.abs(veil.getBoundingClientRect().top)) / 420;
     veil.setAttribute('style', 'opacity: ' + blackValue + ';');
-  });
 
+    // parallax for main section
+    var yPos = -(window.scrollY / main.dataset.speed); 
+    var coords = '50% '+ yPos + 'px';
+    main.setAttribute('style', 'background-position: ' + coords);
+  });
 });
