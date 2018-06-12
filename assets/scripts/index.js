5e1e2f3b0b23aa1ef753022e515b2c5c36ad0d5e
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-06-11 19:20:59 -0700

    seeing if this will fix chrome window resize issue

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,8 +2,25 @@ document.addEventListener('DOMContentLoaded', function(event) {
   var veil = document.querySelector('.veil');
   var main = document.querySelector('main');
   var missionStatement = document.querySelector('.mission-statement');
-  
 
+  // initally set position of background (not sure if this is ideal)
+  var Pos = -(window.scrollY / main.dataset.speed); 
+  var coords = '50% '+ Pos + 'px';
+  main.setAttribute('style', 'background-position: ' + coords);
+
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    var h = window.innerHeight;
+    main.style.height = h + 'px';
+    window.addEventListener('resize', () => {
+      var hh = window.innerHeight;
+      main.style.height = hh + 'px';
+    })
+    window.addEventListener('orientationchange', () => {
+      var hh = window.innerHeight;
+      main.style.height = hh + 'px'
+    })
+  }
+  
   window.addEventListener('scroll', function(event) {
     // make main section fade to black
     var blackValue = (Math.abs(veil.getBoundingClientRect().top)) / 420;
@@ -14,4 +31,5 @@ document.addEventListener('DOMContentLoaded', function(event) {
     var coords = '50% '+ yPos + 'px';
     main.setAttribute('style', 'background-position: ' + coords);
   });
+
 });
